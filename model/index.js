d130473398b99244d7a2a7969f073cadff7ee183
NILESH PATIDAR <35222555+nileshpatidar@users.noreply.github.com>
2020-03-02 18:03:14 +0530

    Update index.js

--- model/index.js
+++ model/index.js
@@ -1,20 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = mongoose.Promise
 require('dotenv').config()
-// mongodb+srv://montu:montu@nepon-pn1rg.mongodb.net/hoookedup?retryWrites=true&w=majority
-
-const mongo_uri = 'mongodb+srv://montu:montu@cluster0-pocft.mongodb.net/hoookedup'
-// mongoose.connect(process.env.MONGODB_URI, {
-//     auth: {
-//         user: process.env.MONGO_USER,
-//         password: process.env.MONGO_PWD
-//     }
-// }, { useNewUrlParser: true, useFindAndModify: false })
-// const db = mongoose.connection;
-// db.on('error', console.log.bind(console, 'connection error'));
-// db.once('open', () => {
-//     console.log('\nSuccessfully connected to Mongo!\n');
-// });
+const mongo_uri = 'Url'
 mongoose.connect(mongo_uri, { useNewUrlParser: true }).then(res => console.log('connected mongodb done')).catch(err => console.log('error in mongodb : ', err))
-
 module.exports = mongoose
